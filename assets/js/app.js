7e20ff7afff18eecdfbb8febd61587ff794dd421
Nils Blum-Oeste <nblumoeste@googlemail.com>
2012-08-15 22:07:17 +0200

    Basic implementation of sign up controller

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,20 @@
 'use strict';
 
+var PomodoroEasyWebsite = angular.module('PomodoroEasyWebsite', []);
+
+function SignUpCtrl($scope, $http) {
+  var url = 'http://localhost:3001/users';
+  $scope.send = function() {
+    var data = { 'user' : { 'email': 'abcdedf@gmx.de'} };
+    $http.post(url, data).success( onSuccess ).error( onError );
+  };
+
+  function onSuccess( response ) {
+    console.log( response );
+  };
+  function onError( respsonse ) {
+    console.log( response );
+  };
+};
+SignUpCtrl.$inject = ['$scope', '$http'];
+
