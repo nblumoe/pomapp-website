6f7ec54a4d08c6458d3fd069da2466326d5a21d5
Nils Blum-Oeste <nblumoeste@googlemail.com>
2012-08-16 09:04:47 +0200

    Sign up widget improved

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,15 +5,19 @@ var PomodoroEasyWebsite = angular.module('PomodoroEasyWebsite', []);
 function SignUpCtrl($scope, $http) {
   var url = 'http://localhost:3001/users';
   $scope.send = function() {
-    var data = { 'user' : { 'email': 'abcdedf@gmx.de'} };
+    var data = { 'user' : { 'email': $scope.email} }
     $http.post(url, data).success( onSuccess ).error( onError );
   };
 
   function onSuccess( response ) {
-    console.log( response );
+    $scope.success = true;
+    $scope.error = false;
+    $scope.errorMessage = '';
   };
-  function onError( respsonse ) {
-    console.log( response );
+  function onError( response ) {
+    $scope.error = true;
+    $scope.success = false;
+    $scope.errorMessage = response.errors.email[0];
   };
 };
 SignUpCtrl.$inject = ['$scope', '$http'];
